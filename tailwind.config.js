d6fa1ac18042c5ef7365152828dacf03a89fb1be
AVVKavvk <kumawatvipin066@gmail.com>
2025-05-25 14:54:31 +0530

    magente

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-const daisyui = require("daisyui");
 
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [daisyui],
+  plugins: [],
   daisyui: {
     themes: ["light", "dark"],
     darkTheme: "dark",
